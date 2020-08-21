d760921f2fe06ebb212f426e4fa6945e114737aa
YangDing <yangding19thu@163.com>
2020-08-21 22:42:12 +0800

    为Tracker.cs中的定位器部分代码添加了大量注释，介绍了原理，并改动了少量代码

--- EDCHost21/Tracker.cs
+++ EDCHost21/Tracker.cs
@@ -680,7 +680,7 @@ namespace EDCHOST21
             public int saturation1Lower;
             public int saturation2Lower;
             public int valueLower;
-            public int areaLower;
+            public int areaLower; // 可能是小车面积的最小值
         }
         public LocConfigs configs;
 
@@ -833,6 +833,7 @@ namespace EDCHOST21
             cam2show = Cv2.GetPerspectiveTransform(camCorners, showCorners);
         }
 
+        //校正摄像机画面？
         public void UpdateCorners(Point2f[] corners, MyFlags myFlags)
         {
             if (corners == null) return;
@@ -891,7 +892,7 @@ namespace EDCHOST21
             //因为被困人员同一时间在场上只有1个，其实只要计算1个坐标变换
             //但是还是将这1个坐标构造成了坐标点列方便调用已有函数
             Point2f[] res = LogicToCamera(new Point2f[] { flags.posPersonStart });
-            //此句存疑（yd）
+            //此句存疑[yd]
             flags.posPersonStart = res[0];
         }
     }
@@ -949,17 +950,38 @@ namespace EDCHOST21
             using (Mat ball = new Mat())
             using (Mat car1 = new Mat())
             using (Mat car2 = new Mat())
+
             //using (Mat merged = new Mat())
+
+            // MatType的组成方式：CV_(位数）+（数据类型）+ C（通道数）
+            // 位数：1个像素点在内存中占据的大小（bit），有 8, 16, 32, 64；
+            // 数据类型：U(unsigned), S(signed), F(float);
+            // 通道数：1（灰度图），2（实数+虚数，用于特殊处理，不常见）
+            //         3（RGB彩色图像）,4（带Alpha通道的RGB图像）
+
+            //本上位机采用的图像格式为 “8位深度无符号整数灰度图”
+
             using (Mat black = new Mat(mat.Size(), MatType.CV_8UC1))
             {
-                //颜色空间转化：从RGB转化为HSV
+                //颜色空间转化：将图片从RGB格式转化为HSV格式
                 //hue色调，sat饱和度，value亮度
                 Cv2.CvtColor(mat, hsv, ColorConversionCodes.RGB2HSV);
+                //为了后面Scalar函数中参数写起来方便
                 MyFlags.LocConfigs configs = localiseFlags.configs;
 
-                //二值化：将位于设定区间内的像素点灰度值设置为255，否则为0
-                //实现了目标颜色和其他颜色的区分
-                //Lower表示范围的最小值，Upper表示范围的最大值
+
+                // 由定义：typedef struct Scalar { 
+                //            double val[4]; 
+                //        } Scalar;
+                // 看出 Scalar是由1~4个数据组成的结构体，对应的是图像的1~4个通道的值
+                // 可以认为 Scalar 是某个像素点（不一定在图像中）
+                // 在下面的代码中，Scalar()中的3个参数分别对应 HSV 图像的 H、S、V 通道
+
+                // InRange函数实现了将图像二值化的功能：
+                // 即将设定值区间内像素点灰度值设置为 255（白），否则为 0（黑）
+                // 从而完成了目标颜色和其他颜色的区分，即生成蒙版(Mask)
+                // Lower表示范围的最小值，Upper表示范围的最大值
+                // 注意：只有H,S,V全部位于给定区间内的像素点才会被设为全白
 
                 //针对小球颜色的二值化
                 Cv2.InRange(hsv,
@@ -977,7 +999,8 @@ namespace EDCHOST21
                     new Scalar(configs.hue2Upper, 255, 255),
                     car2);
 
-                //将二值化图像打印到窗口（蒙版画面，从setwindow中调出）上，以便后续调试
+                // “显示调试蒙版”选项在SetWindow窗口中可以勾选，勾选后 showmask 为 true
+                // 若被勾选，则将二值化图像打印到窗口，以便调试
                 if (localiseFlags.showMask)
                 {
                     Cv2.ImShow("Ball", ball);
@@ -989,21 +1012,30 @@ namespace EDCHOST21
                     Cv2.DestroyAllWindows();
                 }
 
+                // Contour 意即“轮廓”，contours是双重向量，向量内每个元素保存了一组由连续的Point点构成的向量
+                // 每一组Point点集就是一个轮廓。有多少轮廓，向量contours就有多少元素。
                 Point2i[][] contours0, contours1, contours2;
 
-                //图像轮廓识别：根据二值化图象，识别值为255的色块轮廓
-                //参数解释：图像矩阵，扫描外围轮廓，只保留轮廓的拐点
-                //其返回值为轮廓上的拐点
+                // FindContoursAsArray(图像轮廓识别)：根据二值化图象，识别值为255的色块轮廓
+                // 参数解释：1、待处理的图像；2、轮廓检索模式；3、轮廓的近似方法
+                // RetrieveModes.External 表示只检测最外围轮廓，忽略内轮廓
+                // ContourApproximationModes.ApproxSimple 表示仅保存轮廓的拐点信息，不保存所有轮廓点
+                // 返回值：轮廓上的拐点集合
                 contours0 = Cv2.FindContoursAsArray(ball, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
                 contours1 = Cv2.FindContoursAsArray(car1, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
                 contours2 = Cv2.FindContoursAsArray(car2, RetrievalModes.External, ContourApproximationModes.ApproxSimple);
-
+                
                 //根据拐点的图像矩来计算拐点的中心点坐标
                 //小球
                 foreach (Point2i[] c0 in contours0)
                 {
                     Point2i centre = new Point2i();
+                    // Moments表示矩，这是一个概率与统计学上的概念
                     Moments moments = Cv2.Moments(c0);
+                    // Mij = ∑(r * X^i * Y^j)，其中 r = (x, y)为向量
+                    // M00 为面积
+                    // 质心(X0, Y0) = (M10 / M00, M01 / M00)
+                    // 此处计算出拐点的中心点坐标
                     centre.X = (int)(moments.M10 / moments.M00);
                     centre.Y = (int)(moments.M01 / moments.M00);
                     double area = moments.M00;
@@ -1039,15 +1071,12 @@ namespace EDCHOST21
                 foreach (Point2i c2 in centres2) Cv2.Circle(mat, c2, 10, new Scalar(0xff, 0x00, 0x00), -1);
                 if (localiseFlags.gameState != GameState.Unstart)
                 {
-                    for (int i = 0; i < localiseFlags.currPersonNum; ++i)
-                    {
-                        int x10 = localiseFlags.posPersonStart[i].X - 8;
-                        int y10 = localiseFlags.posPersonStart[i].Y - 8;
-                        //在人员起始位置上绘制矩形
-                        Cv2.Rectangle(mat, new Rect(x10, y10, 16, 16), new Scalar(0x00, 0xff, 0x00), -1);
-                    }
-                }
+                    //在人员起始位置上绘制矩形
+                    int x10 = localiseFlags.posPersonStart.X - 8;
+                    int y10 = localiseFlags.posPersonStart.Y - 8;
+                    Cv2.Rectangle(mat, new Rect(x10, y10, 16, 16), new Scalar(0x00, 0xff, 0x00), -1);
 
+                }
                 //Cv2.Merge(new Mat[] { car1, car2, black }, merged);
                 //Cv2.ImShow("binary", merged);
             }
