f41a64f1178911dcac6418603859c1693e2a9724
XuHelin <xhl19@mails.tsinghua.edu.cn>
2020-09-01 21:06:22 +0800

    Laby 构造函数，从文本读取

--- EDCHost21/Labyrinth.cs
+++ EDCHost21/Labyrinth.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,5 +23,21 @@ namespace EDCHOST21
             }
         }
 
+        //Laby. 构造函数 从文本读取
+        public Labyrinth(TextReader reader)
+        {
+            mpWallList = new Wall[16];
+            for (int i = 0; i < 16; i++)
+            {
+                string text = reader.ReadLine();
+                string[] bits = text.Split(' ');
+                int x1 = int.Parse(bits[0]);
+                int y1 = int.Parse(bits[1]);
+                int x2 = int.Parse(bits[2]);
+                int y2 = int.Parse(bits[3]);
+                mpWallList[i] = new Wall(new Dot(x1, y1), new Dot(x2, y2));
+            }
+            Debug.WriteLine("Labyrinth Created from text.");
+        }
     }
 }
