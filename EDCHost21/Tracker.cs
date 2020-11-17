d0943d286548e5580e4f766bef7f8c4ad621787b
鼎 杨 <1057175124@qq.com>
2020-11-17 11:07:21 +0800

    修复了我也不懂为什么的bug

--- EDCHost21/Tracker.cs
+++ EDCHost21/Tracker.cs
@@ -170,10 +170,7 @@ namespace EDCHOST22
 
                 // 启动计时器，执行给迷宫外的小车定时发信息的任务
                 timerMsg100ms.Start();                
-            }
-
-            // 从 labyrinth 目录下读取所有障碍物文件
-            game.mLabyrinth.GetLabyName();
+            }   
 
             Debug.WriteLine("Tracker Initialize Finished\n");
         }
@@ -181,6 +178,9 @@ namespace EDCHOST22
         // 进行界面刷新、读取摄像头图像、与游戏逻辑交互的周期性函数
         private void Flush()
         {
+            // 从 labyrinth 目录下读取所有障碍物文件
+            game.mLabyrinth.GetLabyName();
+
             // 如果还未进行参数设置，则创建并打开SetWindow窗口，进行参数设置
             if (!alreadySet)
             {
